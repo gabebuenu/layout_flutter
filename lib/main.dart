8365a57112dbda3ec91b88c860ca2207f407f5a5
Gabriel Bueno <127902855+gabebuenu@users.noreply.github.com>
2025-03-17 22:13:02 -0300

    att main create task

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,8 @@ class DetalheProduto extends StatelessWidget {
           FotoProduto(imagem: "assets/img/pendrive.png"),
           TituloProduto(nome: "FONE DE OUVIDO BLUETOOTH", preco: 190.00),
           TituloProduto(nome: "FONE DE OUVIDO BLUETOOTH", preco: 210.00),
-          AcoesProduto()  
+          AcoesProduto(),
+          DescricaoProduto(),  
         ],
       ),
     );
@@ -132,6 +133,35 @@ class AcoesProduto extends StatelessWidget {
           icon: Icon(Icons.download)
           ) 
       ],
+    );
+  }
+}
+
+class DescricaoProduto extends StatelessWidget {
+  /*
+  1. adicione um atributo para receber a descrição como um parâmetro do componente.
+
+  2. use este atributo para definir o texto que será exibido.
+  3. coloque o texto dentro de um container com uma borda e um padding de 12 pontos em todos os lados
+  4. faça o texto ficar com um fonte de 18 pontos e justificado
+  5. adicione este widget ao final do DetalheProduto passando sua descrição como parâmetro
+  */
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.lightBlue),
+      ),
+      padding: EdgeInsets.all(12.0),
+      child: Text(
+        "Fone de ouvido Bluetooth com microfone, bateria de longa duração e alta qualidade de som.",
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.normal,
+        ),
+        textAlign: TextAlign.justify,
+      ),
     );
   }
 }
